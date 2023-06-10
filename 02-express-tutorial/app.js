37424d33d5dfcdfaded055f50bb73fec9c4e0ca1
clark rivas <clarkrivas108@gmail.com>
2023-06-10 16:03:04 +0800

    second commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,23 @@
-console.log('Express Tutorial')
+const http = require('http');
+const fs = require('fs');
+const mime = require('mime-types');
+
+const server = http.createServer((req,res)=>{
+    const mimeType = mime.lookup('express.html');
+    if(req.url === '/'){
+        res.writeHead(200, { 'Content-Type': mimeType});
+        console.log(mimeType)
+        fs.readFile('final/express.html', 'utf-8', (err, data) => {
+            res.write(data)
+            res.end()
+          });
+        
+    }else{
+        res.writeHead(404)
+        res.end('404 not found')
+    }
+})
+
+server.listen(5000,()=>{
+    console.log('listening port 5000')
+})
